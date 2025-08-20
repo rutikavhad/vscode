18722161242b412c6341b710de7e6f451fe5aeae
rutikavhad <rutikavhad27@gmail.com>
2025-08-21 00:24:57 +0530

    Clean up strcpy.asm

--- system2/strcpy.asm
+++ system2/strcpy.asm
@@ -1,22 +1,10 @@
 section .data
-	string db "this is string",0
-section .bss
-	string1 resb 30
-section .text
-	global main
-	extern puts
-main:
-	mov esi , string
-	mov edi,string1
-	xor ecx,ecx
-	mov al,byte[edi]
-	mov byte[edi],al
-	inc esi
-	inc edi
-	cmp byte[esi],0
-	jnz lp
 
-	push string1
-	call puts
-	add esp,4
-	ret
+section .text
+global _start
+
+_start:
+    mov eax, [ebx + esi*4]
+    mov ecx, [edi + eax*2]
+    mov edx, [ebx + ecx*1]
+    mov ebx, [esi + edi*8]
