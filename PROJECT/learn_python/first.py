5d5700a3eab7be504232fd27f78b44c61da660d2
rutikavhad <rutikavhad27@gmail.com>
2025-09-01 05:07:00 +0530

    from  windows

---
+++ PROJECT/learn_python/first.py
@@ -0,0 +1,42 @@
+#all about python
+#
+print("hello")
+
+#tuple,string  in python
+
+# programing_lanuages="java","python","c","c++","javascrip"
+
+
+# for lanuage in programing_lanuages:
+#     print(lanuage)
+
+
+#list and for loop in python
+# count=0
+data=[[0,0,0],
+      [0,0,0],
+      [0,0,0],]
+print(" 0  1  2")
+for count ,row in enumerate(data):# enumerate used to count loop we can use this in for loop
+    print(row)
+    #count=count+1
+
+#print(count)
+
+data[0][1]=3
+for item in row :
+    print(item)
+test=[1,2,3,4,5,6,7,8,9,0]
+
+print(data)
+
+
+
+
+#make function in pytjon
+
+def geme():
+    print("hello")
+
+geme()
+ 
