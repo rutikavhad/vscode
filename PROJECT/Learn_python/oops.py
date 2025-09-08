06730d767fe5e7996e7622f7719a403538e254d4
rutikavhad <rutikavhad27@gmail.com>
2025-09-08 15:28:52 +0530

    hello

--- PROJECT/Learn_python/oops.py
+++ PROJECT/Learn_python/oops.py
@@ -15,6 +15,21 @@ a=data()
 a.names()
 a.dis()
 
-from main import printt
+from main import printt  # this used to run any progeram other files
+#main is program name and printt is a function of this main program
 
-printt()
+printt() # this call this printt function from main program
+
+#work with json file most imp for our system
+
+import json
+
+data = '{"user":"matrix","attack":"SQLi"}'
+
+# Convert string → dict
+parsed = json.loads(data)
+print(parsed["attack"])
+
+# Convert dict → JSON string
+new_json = json.dumps({"status": "blocked", "ip": "127.0.0.1"})
+print(new_json)
